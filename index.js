0193caed971c921839803678537af83786083472
hemanth <hemanth.hm@gmail.com>
2015-03-01 08:15:03 +0000

    Better desc in package.json

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ module.exports = function (ctx) {
   var fw = arguments[3] === undefined ? ctx.canvas.width : arguments[3];
   var fh = arguments[4] === undefined ? ctx.canvas.height : arguments[4];
   return (function () {
-    var gradiant = undefined;
+    var gradient = undefined;
 
     ctx.save();
 
-    var fillNrestore = function (ctx, gradiant, fx, fy, fw, fh) {
+    var fillNrestore = function (ctx, gradient, fx, fy, fw, fh) {
       ctx.fillStyle = gradiant;
       ctx.fillRect(fx, fy, fw, fh);
       ctx.restore();
@@ -18,18 +18,18 @@ module.exports = function (ctx) {
 
     return {
       linear: function linear(gx1, gy1, gx2, gy2, colorStops) {
-        gradiant = ctx.createLinearGradient(gx1, gy1, gx2, gy2);
+        gradient = ctx.createLinearGradient(gx1, gy1, gx2, gy2);
         colorStops.forEach(function (colorStop) {
           return gradiant.addColorStop(colorStop.position, colorStop.color);
         });
-        fillNrestore(ctx, gradiant, fx, fy, fw, fh);
+        fillNrestore(ctx, gradient, fx, fy, fw, fh);
       },
       radial: function radial(x0, y0, r0, x1, y1, r1, colorStops) {
-        gradint = ctx.createRadialGradient(x0, y0, r0, x1, y1, r1);
+        gradient = ctx.createRadialGradient(x0, y0, r0, x1, y1, r1);
         colorStops.forEach(function (colorStop) {
           return gradiant.addColorStop(colorStop.position, colorStop.color);
         });
-        fillNrestore(ctx, gradiant, fx, fy, fw, fh);
+        fillNrestore(ctx, gradient, fx, fy, fw, fh);
       }
     };
   })();
